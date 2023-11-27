2b49817216a2ed1e3997cedb34884880d83a5ba2
JL10 <126992358+JLizcano10@users.noreply.github.com>
2023-11-27 18:18:04 -0500

    App component transformed into a class

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+class App extends Component {
+  state = {
+    count: 0,
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'start',
+          alignItems: 'center',
+          fontSize: '30px',
+          color: '#010101',
+          padding: '20px',
+        }}
+      >
+        <h1>Hola Amigos</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
