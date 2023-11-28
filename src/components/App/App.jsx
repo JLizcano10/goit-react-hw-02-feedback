e85bbc099746f6cb64bd06ed11522975efdb0ae8
JL10 <126992358+JLizcano10@users.noreply.github.com>
2023-11-27 23:16:25 -0500

    totalFeedback variable is create for conditional ln 43

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -33,13 +33,14 @@ class App extends Component {
 
   render() {
     const options = Object.keys(this.state);
+    const totalFeedback = this.countTotalFeedback();
     return (
       <Container>
         <Section title={'Please leave feedback'}>
           <FeedbackOptions options={options} increment={this.handleIncrement} />
         </Section>
         <Section title={'Statistics'}>
-          {this.countTotalFeedback() > 0 ? (
+          {totalFeedback > 0 ? (
             <Statistics
               good={this.state.good}
               neutral={this.state.neutral}
